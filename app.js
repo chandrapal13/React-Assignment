42cef0bd904ce5e122afc56c6bddf73c74224ab5
chandrapal singh tomar <56870147+chandrapal13@users.noreply.github.com>
2021-03-25 18:16:00 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,75 @@
-const express = require("express");
- 
-const app = express();
+// create and insert document in mongoose....
 
-  app.get("/", (req,res) => {
- 
-      res.send("<h1>hello<h1>");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/chandrapaul",{ useNewUrlParser: true, useUnifiedTopology: true })
+.then( () => console.log("connection successfull...."))
+.catch((err) => console.log(err)); 
+
+const playlistSchema = new mongoose.Schema({
+name:{ String,
+type: String,
+required: true,
+},
+ctype: String,
+videos: Number,
+author: String,
+active: Boolean,
+Date: {
+
+type : Date,
+default: Date.now
+
+}
 
 
-  });
-
-app.get('/about', (req , res) => {
- 
-  res.send("<h1>WORLD</h1>");
 
 })
-  app.listen(9000, () =>{
-    
-    console,console.log("listining at 7000");
+//Collection creation
+const Playlist = new mongoose.model("Playlist",playlistSchema);
 
- 
-  })
+
+// create and insert document...
+
+
+const createDocument = async () => {
+
+
+  try {
+const reactPlaylist = new Playlist({
+
+
+    name : "Node Js",
+    ctype : "Back End",   
+    videoes : 40,
+    author : "Chandrapaul ",
+    active : true
+    
+    })
+   
+    const result = await reactPlaylist.save();
+    console.log(result); 
+  
+
+  } catch(err){
+
+   console.log(err);
+
+  }
+
+
+  }
+
+// createDocument();
+
+
+
+const getDocument = async () => {
+
+const result = await playlists.find({ctype: "Front"});
+
+console.log(result);
+
+
+} 
+getDocument();
